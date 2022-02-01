3a8478c3e48f094199d97230b01eff3bc7393e56
Santiago Joel Cerdán <santyjoell@gmail.com>
2022-02-01 12:10:29 -0300

    Create ViewModel

---
+++ app/src/main/java/com/example/animefilms/viewmodel/AnimeFilmViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.animefilms.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.animefilms.model.AnimeFilmItem
+import com.example.animefilms.repository.AnimeFilmRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AnimeFilmViewModel @Inject
+constructor(private val repository: AnimeFilmRepo) : ViewModel() {
+
+    private val _response = MutableLiveData<ArrayList<AnimeFilmItem>>()
+
+    fun getResponses(): LiveData<ArrayList<AnimeFilmItem>> {
+        viewModelScope.launch(Dispatchers.IO) {
+            val films = repository.getAnimeFilm()
+            _response.postValue(films.body())
+        }
+        return _response
+    }
+}
